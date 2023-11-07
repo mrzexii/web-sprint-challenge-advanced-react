0ee9cbb5132ffca69de3b06ed6bf9b3fad5a55dc
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-11-07 11:28:10 -0800

    Update AppFunctional.js

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,99 +1,140 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
-const initialMessage = '';
-const initialEmail = '';
-const initialSteps = 0;
-const initialIndex = 4;
-
 export default function AppFunctional(props) {
-  const [message, setMessage] = useState(initialMessage);
-  const [email, setEmail] = useState(initialEmail);
-  const [steps, setSteps] = useState(initialSteps);
-  const [index, setIndex] = useState(initialIndex);
-
-  function getXY() {
-    const X = index % 3 + 1;
-    const Y = Math.floor(index / 3) + 1;
-    return { X, Y };
+  const [XY, setXY] = useState({ X: 2, Y: 2 });
+  const [index, setIndex] = useState(4);
+  const [steps, setSteps] = useState(0);
+  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState('');
+  function getXY(value) {
+    // Calculate the X and Y coordinates based on the current index
+    const x = value % 3 + 1;
+    const y = Math.floor(value / 3) + 1;
+    setXY({ X: x, Y: y });
   }
-
-  function getXYMessage() {
-    const { X, Y } = getXY();
-    return `Coordinates (${X}, ${Y})`;
-  }
-
   function reset() {
-    setMessage(initialMessage);
-    setEmail(initialEmail);
-    setSteps(initialSteps);
-    setIndex(initialIndex);
+    setXY({ X: 2, Y: 2 });
+    setIndex(4);
+    setSteps(0);
+    setMessage('');
+    setEmail('');
   }
-
   function getNextIndex(direction) {
-    let nextIndex = index;
-
     switch (direction) {
       case 'left':
-        nextIndex = index % 3 === 0 ? index : index - 1;
+        if (index % 3 === 0) return index;
+        else {
+          const nextIndex = index - 1;
+          setIndex(nextIndex);
+          return nextIndex;
+        }
+      case 'right':
+        if (index % 3 === 2) return index;
+        else {
+          const nextIndex = index + 1;
+          setIndex(nextIndex);
+          return nextIndex;
+        }
+      case 'up':
+        if (parseInt(index / 3) === 0) return index;
+        else {
+          const nextIndex = index - 3;
+          setIndex(nextIndex);
+          return nextIndex;
+        }
+      case 'down':
+        if (parseInt(index / 3) === 2) return index;
+        else {
+          const nextIndex = index + 3;
+          setIndex(nextIndex);
+          return nextIndex;
+        }
+      default:
+        break;
+    }
+  }
+  function move(evt) {
+    setMessage('');
+    let nextValue;
+    switch (evt) {
+      case 'left':
+        nextValue = getNextIndex('left');
+        if (index === nextValue) setMessage("You can't go left");
+        else {
+          setIndex(nextValue);
+          getXY(nextValue);
+          setSteps(steps + 1);
+        }
         break;
       case 'right':
-        nextIndex = index % 3 === 2 ? index : index + 1;
+        nextValue = getNextIndex('right');
+        if (index === nextValue) setMessage("You can't go right");
+        else {
+          setIndex(nextValue);
+          getXY(nextValue);
+          setSteps(steps + 1);
+        }
         break;
       case 'up':
-        nextIndex = Math.floor(index / 3) === 0 ? index : index - 3;
+        nextValue = getNextIndex('up');
+        if (index === nextValue) setMessage("You can't go up");
+        else {
+          setIndex(nextValue);
+          getXY(nextValue);
+          setSteps(steps + 1);
+        }
         break;
       case 'down':
-        nextIndex = Math.floor(index / 3) === 2 ? index : index + 3;
+        nextValue = getNextIndex('down');
+        if (index === nextValue) setMessage("You can't go down");
+        else {
+          setIndex(nextValue);
+          getXY(nextValue);
+          setSteps(steps + 1);
+        }
         break;
       default:
         break;
     }
-
-    return nextIndex;
   }
-
-  function move(evt) {
-    const nextIndex = getNextIndex(evt);
-    if (nextIndex === index) {
-      setMessage(`You can't go ${evt}`);
-    } else {
-      setMessage('');
-      setIndex(nextIndex);
-      setSteps(steps + 1);
-    }
-  }
-
-  function onChange(evt) {
-    setEmail(evt.target.value);
-  }
-
   function onSubmit(evt) {
     evt.preventDefault();
+  
     if (!email) {
       setMessage('Ouch: email is required');
       return;
     }
-    axios
-      .post('http://localhost:9000/api/result', {
-        email,
-        x: getXY().X,
-        y: getXY().Y,
-        steps,
-      })
-      .then((res) => {
-        setMessage(res.data.message);
-      })
-      .catch((error) => {
-        setMessage('An error occurred while sending the data to the server.');
-      });
+  
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      setMessage('Ouch: email must be a valid email');
+      return;
+    }
+  
+    if (email === 'foo@bar.baz') {
+      setMessage('foo@bar.baz failure #71');
+    } else {
+      axios
+        .post('http://localhost:9000/api/result', {
+          email: email,
+          x: XY.X,
+          y: XY.Y,
+          steps: steps,
+        })
+        .then((res) => {
+          setMessage(res.data.message);
+        })
+        .catch((error) => {
+          setMessage('An error occurred while sending the data to the server.');
+        });
+    }
+  
     setEmail('');
   }
-
+  
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">{getXYMessage()}</h3>
+        <h3 id="coordinates">Coordinates ({XY.X}, {XY.Y})</h3>
         <h3 id="steps">You moved {steps} {steps === 1 ? 'time' : 'times'}</h3>
       </div>
       <div id="grid">
@@ -104,19 +145,17 @@ export default function AppFunctional(props) {
         ))}
       </div>
       <div className="info">
-        <h3 id="message" role="alert">
-          {message}
-        </h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
         <button id="left" onClick={() => move('left')}>LEFT</button>
         <button id="up" onClick={() => move('up')}>UP</button>
         <button id="right" onClick={() => move('right')}>RIGHT</button>
         <button id="down" onClick={() => move('down')}>DOWN</button>
-        <button id="reset" onClick={reset}>reset</button>
+        <button id="reset" onClick={() => reset()}>reset</button>
       </div>
       <form onSubmit={(e) => onSubmit(e)}>
-        <input id="email" type="email" placeholder="type email" value={email} onChange={(e) => onChange(e)} />
+        <input id="email" type="email" placeholder="Type email" value={email} onChange={(e) => setEmail(e.target.value)} />
         <input id="submit" type="submit" value="Submit Email" />
       </form>
     </div>
