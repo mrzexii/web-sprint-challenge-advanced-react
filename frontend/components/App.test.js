dd2f2d3521cd0886a5d4ffdc32d4de09e9173702
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-11-07 11:32:16 -0800

    Update App.test.js

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import AppFunctional from './AppFunctional';
@@ -5,53 +6,30 @@ import AppFunctional from './AppFunctional';
 test('Renders AppFunctional component without errors', () => {
   render(<AppFunctional />);
 });
-
 test('Displays initial coordinates and steps', () => {
   render(<AppFunctional />);
   expect(screen.getByText('Coordinates (2, 2)')).toBeInTheDocument();
   expect(screen.getByText('You moved 0 times')).toBeInTheDocument();
 });
-
-test('Button labels are rendered on the screen', () => {
-  render(<AppFunctional />);
-  expect(screen.getByText('LEFT')).toBeInTheDocument();
-  expect(screen.getByText('UP')).toBeInTheDocument();
-  expect(screen.getByText('RIGHT')).toBeInTheDocument();
-  expect(screen.getByText('DOWN')).toBeInTheDocument();
-  expect(screen.getByText('reset')).toBeInTheDocument();
-  expect(screen.getByText('Submit Email')).toBeInTheDocument();
-});
-
 test('Typing in the email input changes its value', () => {
   render(<AppFunctional />);
-  const emailInput = screen.getByRole('textbox', { name: 'type email' });
+  const emailInput = screen.getByRole('textbox', { name: 'Type email' });
   fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
   expect(emailInput).toHaveValue('test@example.com');
 });
-
-test('Input value changes when typing', () => {
-  render(<AppFunctional />);
-  const emailInput = screen.getByRole('textbox', { name: 'type email' });
-  fireEvent.change(emailInput, { target: { value: 'new-email@example.com' } });
-  expect(emailInput).toHaveValue('new-email@example.com');
-});
-
-test('Displays the error message for no email', () => {
+test('Displays error message on missing email', () => {
   render(<AppFunctional />);
   const submitButton = screen.getByRole('button', { name: 'Submit Email' });
   fireEvent.click(submitButton);
   expect(screen.getByText('Ouch: email is required')).toBeInTheDocument();
 });
-
-test('Displays the error message for a banned email', async () => {
+test('Reset button resets the component', () => {
   render(<AppFunctional />);
-  const emailInput = screen.getByRole('textbox', { name: 'type email' });
-  const submitButton = screen.getByRole('button', { name: 'Submit Email' });
-
-  fireEvent.change(emailInput, { target: { value: 'foo@bar.baz' } });
-  fireEvent.click(submitButton);
-
-  const errorMessage = await screen.findByRole('alert');
-
-  expect(errorMessage).toHaveTextContent(/foo@bar.baz/i);
+  const emailInput = screen.getByRole('textbox', { name: 'Type email' });
+  fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+  const resetButton = screen.getByRole('button', { name: 'reset' });
+  fireEvent.click(resetButton);
+  expect(emailInput).toHaveValue('');
+  expect(screen.getByText('Coordinates (2, 2)')).toBeInTheDocument();
+  expect(screen.getByText('You moved 0 times')).toBeInTheDocument();
 });
