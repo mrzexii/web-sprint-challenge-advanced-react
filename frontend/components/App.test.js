40e3c2a20037c71efbc51ec73e3f331bab5bec9d
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-11-06 17:25:25 -0800

    Update App.test.js

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,27 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import AppClass from './AppClass'; // You can import the component you want to test here
+
+test('renders headings, buttons, and links', () => {
+  const { getByText, getByTestId } = render(<AppClass />);
+  
+  // Test that the headings, buttons, and links are visible on the screen
+  expect(getByText('Coordinates (2, 2)')).toBeInTheDocument();
+  expect(getByText('You moved 0 times')).toBeInTheDocument();
+  expect(getByTestId('left')).toBeInTheDocument();
+  expect(getByTestId('up')).toBeInTheDocument();
+  expect(getByTestId('right')).toBeInTheDocument();
+  expect(getByTestId('down')).toBeInTheDocument();
+  expect(getByTestId('reset')).toBeInTheDocument();
+});
+
+test('changes input value when typing', () => {
+  const { getByTestId } = render(<AppClass />);
+  const emailInput = getByTestId('email');
+  
+  // Test that typing in the email input changes its value
+  fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+  expect(emailInput.value).toBe('test@example.com');
+});
+
+// You can add more tests here
