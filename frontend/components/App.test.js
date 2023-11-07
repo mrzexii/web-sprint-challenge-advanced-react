7a8deb76c45179caa501afaec08dcfdc797f0e9b
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-11-07 10:12:47 -0800

    Update App.test.js

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import AppFunctional from './AppFunctional';
 
-
 test('Renders AppFunctional component without errors', () => {
   render(<AppFunctional />);
 });
+
+
 test('Displays initial coordinates and steps', () => {
   render(<AppFunctional />);
   expect(screen.getByText('Coordinates (2, 2)')).toBeInTheDocument();
@@ -14,21 +15,14 @@ test('Displays initial coordinates and steps', () => {
 
 test('Typing in the email input changes its value', () => {
   render(<AppFunctional />);
-  const emailInput = screen.getByRole('textbox', { name: 'Type email' });
+  const emailInput = screen.getByRole('textbox', { name: 'type email' });
   fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
   expect(emailInput).toHaveValue('test@example.com');
 });
 
-test('Displays error message on missing email', () => {
-  render(<AppFunctional />);
-  const submitButton = screen.getByRole('button', { name: 'Submit Email' });
-  fireEvent.click(submitButton);
-  expect(screen.getByText('Ouch: email is required')).toBeInTheDocument();
-});
-
 test('Reset button resets the component', () => {
   render(<AppFunctional />);
-  const emailInput = screen.getByRole('textbox', { name: 'Type email' });
+  const emailInput = screen.getByRole('textbox', { name: 'type email' });
   fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
 
   const resetButton = screen.getByRole('button', { name: 'reset' });
@@ -37,4 +31,16 @@ test('Reset button resets the component', () => {
   expect(emailInput).toHaveValue('');
   expect(screen.getByText('Coordinates (2, 2)')).toBeInTheDocument();
   expect(screen.getByText('You moved 0 times')).toBeInTheDocument();
+});
+
+
+test('Displays error message on invalid email submission', () => {
+  render(<AppFunctional />);
+  const emailInput = screen.getByRole('textbox', { name: 'type email' });
+  const submitButton = screen.getByRole('button', { name: 'Submit Email' });
+
+  fireEvent.change(emailInput, { target: { value: 'invalid-email' } });
+  fireEvent.click(submitButton);
+
+  expect(screen.getByText('Ouch: email must be a valid email')).toBeInTheDocument();
 });
