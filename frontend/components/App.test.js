9771b6f3bae3e05692a9b42a28aef342694527a1
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-11-08 11:50:57 -0800

    Update App.test.js

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -8,22 +8,22 @@ test('Renders AppFunctional component without errors', () => {
 
 test('Renders AppFunctional component with coordinates and step count', () => {
   render(<AppFunctional />);
-  expect(screen.getByText(/Coordinates \(\d, \d\)/i)).toBeInTheDocument();
-  expect(screen.getByText(/You moved \d+ times/i)).toBeInTheDocument();
+  expect(screen.getByText('Coordinates (2, 2)')).toBeInTheDocument();
+  expect(screen.getByText('You moved 0 times')).toBeInTheDocument();
 });
 
 test('Renders AppFunctional component with message', () => {
   render(<AppFunctional />);
-  expect(screen.getByText(/An initial message/i)).toBeInTheDocument();
+  expect(screen.getByText('An initial message')).toBeInTheDocument();
 });
 
 test('Renders AppFunctional component with buttons', () => {
   render(<AppFunctional />);
-  expect(screen.getByText(/LEFT/i)).toBeInTheDocument();
-  expect(screen.getByText(/UP/i)).toBeInTheDocument();
-  expect(screen.getByText(/RIGHT/i)).toBeInTheDocument();
-  expect(screen.getByText(/DOWN/i)).toBeInTheDocument();
-  expect(screen.getByText(/reset/i)).toBeInTheDocument();
+  expect(screen.getByText('LEFT')).toBeInTheDocument();
+  expect(screen.getByText('UP')).toBeInTheDocument();
+  expect(screen.getByText('RIGHT')).toBeInTheDocument();
+  expect(screen.getByText('DOWN')).toBeInTheDocument();
+  expect(screen.getByText('reset')).toBeInTheDocument();
 });
 
 test('Typing on the input changes its value', () => {
@@ -40,13 +40,32 @@ test('Renders AppFunctional component with submit button', () => {
 
 test('Check for custom error message', () => {
   render(<AppFunctional />);
+expect(screen.getByText(/Ouch: email must be a valid email/i).toBeInTheDocument();
+});
 
-  // Write a custom matcher function
-  const customMatcher = (content, element) => {
-    return element.textContent.includes(content);
-  };
+test('Submit does not reset coordinates nor steps', () => {
+  render(<AppClass />);
 
-  // Use the custom matcher to find the error message
-  const errorMessage = 'Ouch: email must be a valid email';
-  expect(screen.getByText((content, element) => customMatcher(errorMessage, element))).toBeInTheDocument();
+  // Perform actions: up, right, type valid email, submit
+  const upButton = screen.getByText('UP');
+  const rightButton = screen.getByText('RIGHT');
+  const emailInput = screen.getByPlaceholderText('Type email');
+  const submitButton = screen.getByText('Submit Email');
+
+  fireEvent.click(upButton);
+  fireEvent.click(rightButton);
+  fireEvent.change(emailInput, { target: { value: 'lady@gaga.com' } });
+  fireEvent.click(submitButton);
+
+  // Wait for the success message
+  const successMessage = screen.getByText(/lady win #\d+/i);
+  expect(successMessage).toBeInTheDocument();
+
+  // Verify that coordinates and steps remain unchanged
+  const coordinates = screen.getByText(/Coordinates \(\d, \d\)/i);
+  const steps = screen.getByText(/You moved \d+ times/i);
+
+  // You may adjust the expected values as per your component's behavior
+  expect(coordinates.textContent).toMatch(/\(3.*1\)$/);
+  expect(steps.textContent).toBe('You moved 2 times');
 });
